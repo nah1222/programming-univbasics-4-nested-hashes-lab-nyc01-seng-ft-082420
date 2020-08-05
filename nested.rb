dd89148eb4d745855e3c8e31b2f0c1643b47530a
practical-kernel-3029 <nah1222@yahoo.com>
2020-08-05 16:09:59 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -15,7 +14,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,6 +35,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+  programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -65,8 +65,8 @@ def adding_matz
 #   :languages => ["LISP", "C"]
 # }
 # return the entire updated hash
-
-	programmer_hash = 
+  
+  programmer_hash =
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -81,7 +81,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  
+  programmer_hash[:yukihiro_matsumoto] ={:known_for => "Ruby",:languages => ["LISP, C"]}
+  programmer_hash
+  
 end
 
 def changing_alan
@@ -103,6 +106,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    programmer_hash
 
 end
 
@@ -125,5 +130,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages][1] = ["Assembly"]
+    programmer_hash
 end
